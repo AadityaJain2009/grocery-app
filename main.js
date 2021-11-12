1356f762bcb21312c2ae5ad6d4925e9d81d1481d
AadityaJain2009 <81620807+AadityaJain2009@users.noreply.github.com>
2021-11-12 20:57:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+
+allCostRice1 =  "Cost of 5kg rice in Bigbasket , 348 "; 
+allCostRice2 = "Cost of 5kg rice in Dmart ,  348";
+allCostRice3 = "Cost of 5kg rice  in Bigbazar ,  569";
+allCostRice4 = "Cost of 5kg rice  in Amazon , 390";
+allCostRice5 = "Cost of 5kg rice  in Grofers , 289";
+
+
+function rice() {
+   
+   document.getElementById("bb_cost").innerHTML = " 348";
+   document.getElementById("dm_cost").innerHTML = "348";
+   document.getElementById("bbz_cost").innerHTML = "569";
+   document.getElementById("ama_cost").innerHTML = "390";
+   document.getElementById("gro_cost").innerHTML = "289";
+} 
+
+
+
+function Wheat() {
+    
+    document.getElementById("bb_cost1").innerHTML = " 243";
+   document.getElementById("dm_cost1").innerHTML = "190";
+   document.getElementById("bbz_cost1").innerHTML = "189";
+   document.getElementById("ama_cost1").innerHTML = "239";
+   document.getElementById("gro_cost1").innerHTML = "289";
+ }
+ 
+
+ function sugar() {
+
+   document.getElementById("bb_cost2").innerHTML = " 234";
+   document.getElementById("dm_cost2").innerHTML = "190";
+   document.getElementById("bbz_cost2").innerHTML = "239";
+   document.getElementById("ama_cost2").innerHTML = "220";
+   document.getElementById("gro_cost2").innerHTML = "251";
+ }
+
+
+
+ //    <div class="dropdown , container">
+ //<form action="/action_page.php">
+            
+ //<select name="Grocery" id="gro">
+ //  <option id="Rice"  onclick="rice()">Rice</option>
+  // <option value="Wheat" onclick="Wheat()">Wheat</option>
+  // <option value="Sugar" onclick="sugar()">Suger</option>
+ //</select>
+ //<br><br>
+//</form>
+//</div>
